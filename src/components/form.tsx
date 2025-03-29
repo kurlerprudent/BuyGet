a8d877d46b31c3d7e249ec74d6bde68f910ac9a2
Prudentkurler <sarkodiekurler@gmail.com>
2025-03-29 08:44:28 +0000

    update

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -184,7 +184,7 @@ export default function RegistrationForm() {
             type="submit"
             className="w-full bg-indigo-600 text-white py-3 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
-            Submit Application
+            Submit 
           </button>
         </form>
       </div>
