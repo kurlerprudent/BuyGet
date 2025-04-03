68d6f43920d2e1b1df420d637916e315f7ad892f
Prudentkurler <sarkodiekurler@gmail.com>
2025-04-03 15:25:31 +0000

    Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.buyget.co', // Replace with your actual domain
+    generateRobotsTxt: true, // (optional) Generate a robots.txt file
+    // optional: additional options
+    // exclude: ['/secret-page'], // Exclude specific routes
+    // sitemapSize: 7000, // Split sitemap if you have many URLs
+  };
+  
