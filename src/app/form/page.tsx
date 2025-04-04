d115f21d347340155bf29dbf2f0c93b9a5771134
Prudentkurler <sarkodiekurler@gmail.com>
2025-04-04 18:09:09 +0000

    Checked the link issue is the home page

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,4 +1,11 @@
 import RegistrationForm from "@/components/form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    alternates:{
+        canonical:"https://buyget.co"
+    }
+}
 
 export default function FormPage(){
     return(
